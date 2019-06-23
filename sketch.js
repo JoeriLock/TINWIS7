15cd83863d434803b99ac9d7e60c5a82e6aa13e3
Joeri <joeri.lock@basic-fit.com>
2019-06-23 20:45:00 +0200

    Options to select different disk

--- sketch.js
+++ sketch.js
@@ -1,23 +1,34 @@
 var collisionHandler;
 
-var horSpeed = 3;
+var horSpeed = 0;
 var verSpeed = 0;
 var pause = false;
 var disk1,disk2;
+var selectedDisk = null;
 
 function updateGame(){
   collideDebug(true);
   horSpeed = parseInt(document.getElementById("horSpeed").value,10);
   verSpeed = parseInt(document.getElementById("verSpeed").value,10);
-  setup()
+
+  disk1.horSpeed = horSpeed;
+  disk1.verSpeed = verSpeed;
+}
+
+function displayText(){
+  if(selectedDisk == disk1){
+    var text = "Disk1";
+  }else{
+    var text = "Disk2";
+  }
+  document.getElementById("infoText").innerHTML = "selectedDisk: "+text
 }
 
 function setup(){
   createCanvas(CANVASWIDTH,CANVASHEIGHT);
   disk1 = new Disk(randomPosV(),randomPosH(),50,horSpeed,verSpeed);
-  //disk2 = new Disk(randomPosV(),randomPosH(),50,0,0);
-  disk2 = new Disk(400,200,50,0,0);
-
+  disk2 = new Disk(randomPosV(),randomPosH(),50,0,0);
+  selectedDisk = disk1;
   collisionHandler = new CollisionHandler(disk1,disk2);
 }
 //pause with space
@@ -30,14 +41,18 @@ function keyPressed(){
   if(key == '2'){
     console.log(" x:" + disk2.hor + " y:" + disk2.ver
     + " xSpeed:" + disk2.horSpeed + " ySpeed:" + disk2.verSpeed)
-
+  }
+  if(key == 'a'){
+    selectedDisk = selectedDisk==disk1 ? disk2 : disk1;
+    displayText();
   }
 }
 function mouseClicked(){
+
     if(mouseX < CANVASWIDTH && mouseY < CANVASHEIGHT){
       console.log("mouse " + mouseX +","+mouseY)
-      disk1.changePosition(mouseX,mouseY);
-      disk1.changeSpeed(horSpeed,verSpeed);
+      selectedDisk.changePosition(mouseX,mouseY);
+      selectedDisk.changeSpeed(horSpeed,verSpeed);
     }
 }
 
